03a297fa1defc9d25b0e1f1099119efb9f4e19f2
John <134107082+jksnook@users.noreply.github.com>
2023-10-22 12:10:13 -0700

    create general info form section

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import GeneralInfo from "./GeneralInfo"
+
+function App () {
+  return(
+    <div>
+      <GeneralInfo />
+      {/* <Education /> */}
+      {/* <Experience /> */}
+    </div>
+  )  
+}
+
+export default App
---
+++ src/components/GeneralInfo.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+function GeneralInfo () {
+  const [submitted, setSubmitted] = useState(false);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+
+  function handleEdit () {
+    setSubmitted(false);
+  }
+
+  if (submitted) {
+    return (
+      <div>
+        <ul>
+          <li>{firstName}</li>
+          <li>{lastName}</li>
+          <li>{email}</li>
+        </ul>
+        <button type="button" onClick={handleEdit}>Edit</button>
+      </div>
+    );
+  }
+
+  function handleSubmit () {
+    setSubmitted(true);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>General Info</h2>
+      <ul>
+        <li>
+          <label htmlFor="firstName">First Name</label>
+          <input id="firstName" name="firstName" type="text" value={firstName} onChange={(e) => {setFirstName(e.target.value)}} />
+        </li>
+        <li>
+          <label htmlFor="lastName">Last Name</label>
+          <input id="lastName" name="lastName" type="text" value={lastName} onChange={(e) => {setLastName(e.target.value)}} />
+        </li>
+        <li>
+          <label htmlFor="email">Email</label>
+          <input  id="email" name="email" type="email" value={email} onChange={(e) => {setEmail(e.target.value)}} />
+        </li>
+      </ul>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default GeneralInfo;
